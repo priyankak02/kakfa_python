86cdafcdd581e79d273cd564e3372a70e3378630
priyankak02 <125034116+priyankak02@users.noreply.github.com>
2023-06-08 10:41:03 +0530

    Update consumer.py

--- simple-python/consumer.py
+++ simple-python/consumer.py
@@ -4,3 +4,6 @@ consumer = KafkaConsumer('my-topic',bootstrap_servers=['localhost:9092'])
 
 for m in consumer:
         print(m.value)
+        print ("%s:%d:%d: key=%s value=%s" % (m.topic, m.partition,
+                                          m.offset, m.key,
+                                          m.value))
